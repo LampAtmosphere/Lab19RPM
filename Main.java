e8cfde150c1aa3be283af54e1f3dafd87f83f16a
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2023-06-13 09:41:46 +0300

    Сделал всё номного легче

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите 2 числа через 'enter':");
+        Double num1 = sc.nextDouble();
+        Double num2 = sc.nextDouble();
+        if (num2 == 0){System.out.println("На ноль делить нельзя");
+        return;}
+        Singleton result = Singleton.getInstance();
+
+
+        System.out.println(result.Calculator(num1,num2));
+    }
+}
---
+++ Singleton.java
@@ -0,0 +1,12 @@
+public class Singleton {
+private static Singleton instance;
+private Singleton(){}
+public static Singleton getInstance(){
+    if (instance==null){
+        instance = new Singleton();}
+    return instance;
+}
+    public double Calculator(double x, double y) {
+        return 2 * x + 3 / y;
+    }
+}
